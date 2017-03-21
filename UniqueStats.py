86eadc75481d033852b5cdbb33ac26d43e7dc127
Allan Dong <allan.w.dong@gmail.com>
2017-03-21 14:22:55 -0400

    csv

---
+++ UniqueStats.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 17 20:44:26 2017
+
+@author: allan
+"""
+
+import os
+import json
+
+
+master_csv_dir = '/home/allan/Tuberculosis/CSVs'
+hash_dir = '/home/allan/Tuberculosis/Hash'
+testhash = '/home/allan/Tuberculosis/Hash/Test'
+
+
+csvdir = master_csv_dir
+os.chdir(hash_dir)
+
+
+test = open(testhash, 'r')
+# Creates dict from Hash2 and Hash3 files, contains 'u' (unicode string 
+#  prefix) in dict
+
+Hash2 = json.load(open('/home/allan/Tuberculosis/Hash/Hash2', 'r'))
+Hash3 = json.load(open('/home/allan/Tuberculosis/Hash/Hash3', 'r'))
+Test = json.load(test)
+
+'''
+def UniqueNaiveBayes(Hash2address, Hash3address):
+    Hash2 = json.load(open(Hash2address, 'r'))
+    Hash3 = json.load(open(Hash3address, 'r'))
+    for pair in Hash3:
+        
+''' 
